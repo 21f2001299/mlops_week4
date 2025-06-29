c0b5a4cd4c4935fd105ba996b849ba2788a281a5
Shibasis mukherjee <109910204+21f2001299@users.noreply.github.com>
2025-06-29 20:44:56 +0530

    Update test_data_validation.py

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -1,11 +1,13 @@
+import pytest
 import pandas as pd
 
-def test_data_shape():
-        df = pd.read_csv("../iris.csv")
-        assert df.shape[1] == 5
-        assert df.shape[0] > 0
-                
-def test_no_missing_values():
-        df = pd.read_csv("../iris.csv")
-        assert df.isnull().sum().sum() == 0, "Missing values found"
+@pytest.fixture(scope="module")
+def iris_df():
+    return pd.read_csv("../iris.csv")
 
+def test_data_shape(iris_df):
+    assert iris_df.shape[1] == 5
+    assert iris_df.shape[0] > 0
+
+def test_no_missing_values(iris_df):
+    assert iris_df.isnull().sum().sum() == 0, "Missing values found"
