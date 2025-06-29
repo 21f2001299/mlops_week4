b7dff77601d6dee77b6ddba3b48183d8f5623d6b
Shibasis mukherjee <109910204+21f2001299@users.noreply.github.com>
2025-06-29 20:22:19 +0530

    Update test_data_validation.py

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 def test_data_shape():
         df = pd.read_csv("../iris.csv")
-        assert df.shape == (150, 5), "Expected shape (150, 5)"
+        assert df.shape[1] == 5
+        assert df.shape[0] > 0
                 
 def test_no_missing_values():
         df = pd.read_csv("../iris.csv")
